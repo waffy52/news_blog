26284f92a4f5182c24f653cf764a01800472372d
Stefan Faix <stefan.faix@accenture.com>
2020-08-10 12:05:37 +0200

    eslint setup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const router = express.Router();
 
@@ -6,17 +6,17 @@ const path = __dirname + '/views/';
 const port = 8000;
 
 router.use(function (req,res,next) {
-    console.log('/' + req.method);
-    next();
-  });
+  console.log('/' + req.method);
+  next();
+});
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // });
 
 router.get('/',function(req,res){
-    res.sendFile(path + 'index.html');
-  });
+  res.sendFile(path + 'index.html');
+});
 
 
 
@@ -27,5 +27,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path));
 
 app.listen(port, () => {
-  console.log(`News blog listening on port ${port}!`)
+  console.log(`News blog listening on port ${port}!`);
 });
