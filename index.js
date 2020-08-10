992a124a3dcdf67c53a245d2ee9b7be821c5fcc4
Stefan Faix <stefan.faix@accenture.com>
2020-08-10 15:39:50 +0200

    remove html extension from links

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const path = __dirname + '/views/';
 const port = 8000;
 
-router.use(function (req,res,next) {
+router.use(function (req, res, next) {
   console.log('/' + req.method);
   next();
 });
@@ -14,17 +14,33 @@ router.use(function (req,res,next) {
 //   res.send('Hello World!')
 // });
 
-router.get('/',function(req,res){
+router.get('/', function(req, res){
   res.sendFile(path + 'index.html');
 });
 
+router.get('/index', function(req, res){
+  res.sendFile(path + 'index.html');
+});
 
+router.get('/about', function(req, res){
+  res.sendFile(path + 'about.html');
+});
 
-// app.set('view engine', 'html');
+router.get('/post', function(req, res){
+  res.sendFile(path + 'post.html');
+});
+
+router.get('/contact', function(req, res){
+  res.sendFile(path + 'contact.html');
+});
+
+app.set('view engine', 'html');
 // this will enable our application to parse our user’s entered data.
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path));
+app.use('/', router);
+
 
 app.listen(port, () => {
   console.log(`News blog listening on port ${port}!`);
