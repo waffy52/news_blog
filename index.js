630952851f39033906a4355e4cc9ec14a6af7359
Stefan Faix <stefan.faix@accenture.com>
2020-08-10 11:51:50 +0200

    import views

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+const path = __dirname + '/views/';
+const port = 8000;
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+router.use(function (req,res,next) {
+    console.log('/' + req.method);
+    next();
+  });
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// });
+
+router.get('/',function(req,res){
+    res.sendFile(path + 'index.html');
+  });
+
+
+
+// app.set('view engine', 'html');
+// this will enable our application to parse our user’s entered data.
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path));
+
+app.listen(port, () => {
+  console.log(`News blog listening on port ${port}!`)
 });
